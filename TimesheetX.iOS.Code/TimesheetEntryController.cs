c4ebae4d423b04780fa0e0ce92eaea528fe8cf27
Benjamin Tam <ben@teamtam.com>
2015-10-30 09:11:38 +1100

    Resharper-ing

--- TimesheetX.iOS.Code/TimesheetEntryController.cs
+++ TimesheetX.iOS.Code/TimesheetEntryController.cs
@@ -16,6 +16,23 @@ namespace TimesheetX.iOS.Code
         {
             base.ViewDidLoad();
 
+            CreateDateLabel();
+            CreateDateField();
+            CreateCustomerLabel();
+            CreateCustomerField();
+            CreateProjectLabel();
+            CreateProjectField();
+            CreateHoursLabel();
+            CreateHoursField();
+            CreateCommentLabel();
+            CreateCommentField();
+            CreateSickLeaveLabel();
+            CreateSickLeaveField();
+            CreateSubmitButton();
+        }
+
+        private void CreateDateLabel()
+        {
             var dateLabel = new UILabel()
             {
                 Frame = new CGRect(20, 40, 80, 32),
@@ -24,7 +41,10 @@ namespace TimesheetX.iOS.Code
                 Text = "Date"
             };
             View.AddSubview(dateLabel);
+        }
 
+        private void CreateDateField()
+        {
             var dateField = new UILabel()
             {
                 Frame = new CGRect(100, 40, View.Bounds.Width - 120, 32),
@@ -33,7 +53,10 @@ namespace TimesheetX.iOS.Code
                 Text = "10/10/2015"
             };
             View.AddSubview(dateField);
+        }
 
+        private void CreateCustomerLabel()
+        {
             var customerLabel = new UILabel()
             {
                 Frame = new CGRect(20, 80, 80, 32),
@@ -42,7 +65,10 @@ namespace TimesheetX.iOS.Code
                 Text = "Customer"
             };
             View.AddSubview(customerLabel);
+        }
 
+        private void CreateCustomerField()
+        {
             var customerField = new UILabel()
             {
                 Frame = new CGRect(100, 80, View.Bounds.Width - 120, 32),
@@ -51,7 +77,10 @@ namespace TimesheetX.iOS.Code
                 Text = "Some customer"
             };
             View.AddSubview(customerField);
+        }
 
+        private void CreateProjectLabel()
+        {
             var projectLabel = new UILabel()
             {
                 Frame = new CGRect(20, 120, 80, 32),
@@ -60,7 +89,10 @@ namespace TimesheetX.iOS.Code
                 Text = "Project"
             };
             View.AddSubview(projectLabel);
+        }
 
+        private void CreateProjectField()
+        {
             var projectField = new UILabel()
             {
                 Frame = new CGRect(100, 120, View.Bounds.Width - 120, 32),
@@ -69,7 +101,10 @@ namespace TimesheetX.iOS.Code
                 Text = "Some project"
             };
             View.AddSubview(projectField);
+        }
 
+        private void CreateHoursLabel()
+        {
             var hoursLabel = new UILabel()
             {
                 Frame = new CGRect(20, 160, 80, 32),
@@ -78,7 +113,10 @@ namespace TimesheetX.iOS.Code
                 Text = "Hours"
             };
             View.AddSubview(hoursLabel);
+        }
 
+        private void CreateHoursField()
+        {
             var hoursField = new UIPickerView()
             {
                 Frame = new CGRect(100, 160, View.Bounds.Width - 120, 40),
@@ -86,7 +124,10 @@ namespace TimesheetX.iOS.Code
             hoursField.Model = new TimesheetEntryHoursViewModel();
             hoursField.Select(32, 0, true);
             View.AddSubview(hoursField);
+        }
 
+        private void CreateCommentLabel()
+        {
             var commentLabel = new UILabel()
             {
                 Frame = new CGRect(20, 200, 80, 32),
@@ -95,7 +136,10 @@ namespace TimesheetX.iOS.Code
                 Text = "Comment"
             };
             View.AddSubview(commentLabel);
+        }
 
+        private void CreateCommentField()
+        {
             var commentField = new UITextField()
             {
                 Frame = new CGRect(100, 200, View.Bounds.Width - 120, 32),
@@ -104,7 +148,10 @@ namespace TimesheetX.iOS.Code
                 Placeholder = ""
             };
             View.AddSubview(commentField);
+        }
 
+        private void CreateSickLeaveLabel()
+        {
             var sickLeaveLabel = new UILabel()
             {
                 Frame = new CGRect(20, 240, 80, 32),
@@ -113,7 +160,10 @@ namespace TimesheetX.iOS.Code
                 Text = "Sick Leave"
             };
             View.AddSubview(sickLeaveLabel);
+        }
 
+        private void CreateSickLeaveField()
+        {
             var sickLeaveField = new UISwitch()
             {
                 Frame = CGRect.Empty
@@ -121,7 +171,10 @@ namespace TimesheetX.iOS.Code
             sickLeaveField.Transform = CGAffineTransform.MakeScale((nfloat)1, (nfloat)1);
             sickLeaveField.Frame = new CGRect(View.Bounds.Width - sickLeaveField.Frame.Size.Width - 20, 240, sickLeaveField.Frame.Size.Width, sickLeaveField.Frame.Size.Height);
             View.AddSubview(sickLeaveField);
+        }
 
+        private void CreateSubmitButton()
+        {
             var submitButton = new UIButton(UIButtonType.System)
             {
                 Frame = new CGRect(20, 280, View.Bounds.Width - 40, 32),
